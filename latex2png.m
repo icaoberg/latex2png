d3ea6ba2268f3dd25109aae7c1a90264e6174be4
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2015-02-12 14:56:15 -0500

    Updated main script

--- latex2png.m
+++ latex2png.m
@@ -1,9 +1,10 @@
-function latex2png( snippet, outfile )
+function latex2png( snippet, param )
+% LATEX2PNG
 
 % Author: Ivan E. Cao-Berg (icaoberg@cmu.edu)
 % Created: May 21, 2008
 %
-% Copyright (C) 2008 Ivan E. Cao-Berg
+% Copyright (C) 2008-2015 Ivan E. Cao-Berg
 % Carnegie Mellon University
 %
 % This program is free software; you can redistribute it and/or modify
@@ -23,50 +24,114 @@ function latex2png( snippet, outfile )
 %
 % For additional information visit http://latex2png.sourceforge.net
 
-%Pre-requisites
-latex = '/usr/bin/latex';
-dvipng = '/usr/bin/dvipng';
-convert = '/usr/bin/convert';
-
-%choose a number from 75-300
-density = 300;
-
-if( ~exist( latex, 'file' ) )
-    error( ['latex2png: LaTeX was not located in ' latex '. To install ' ...
-        'LaTeX in Ubuntu type in terminal: sudo apt-get install ' ...
-        'tetex-base tetex-extra'] );
-elseif( ~exist( dvipng, 'file' ) )
-    error( ['latex2png: dvipng was not located in ' dvipng '. To install ' ...
-        'dvipng in Ubuntu type in terminal: sudo apt-get install ' ...
-        'dvipng'] );
-elseif( ~exist( convert, 'file' ) )
-    error( ['latex2png: convert was not located in ' convert '. To install ' ...
-        'convert in Ubuntu type in terminal: sudo apt-get install ' ...
-        'imagemagick'] );
-elseif( nargin ~= 2 )
-    error( ['latex2png: Wrong number of input arguments. Refer to ' ... 
-        'documentation or visit http://latex2png.sourceforge.net for ' ...
-        'more information']);
-else
-    %write temporary file
-    writeLaTeXfile( snippet, outfile );
-    
-    %create dvi file
-    system( [latex ' -interaction=nonstopmode ' outfile ' .tex'] );
-    
-    %parse dvi to png
-    %if you want to make your image transparent change white to transparent
-    system( [dvipng ' -q -T tight -bg white -D' density ' ' ...
-        outfile '.dvi'] );
-    
-    %remove temporary files
-    system( ['rm ' outfile '.tex ' outfile '.dvi ' outfile '.aux ' ...
-        outfile '.log' ] );
+%%%%%%%%%%%%%%%%%%%%%%%%%
+% CHECK INPUT ARGUMENTS %
+%%%%%%%%%%%%%%%%%%%%%%%%%
+if ~isunix()
+    warning('This script has only been tested in Ubuntu 14.01.');
 end
 
+if nargin == 1
+    param = [];
+    debug = false;
+    
+    %pre-requisites
+    outfile = 'snippet.png';
+    latex = '/usr/bin/latex';
+    dvipng = '/usr/bin/dvipng';
+    convert = '/usr/bin/convert';
+    
+    %choose a number from 75-300
+    density = 300;
+elseif nargin == 2
+    %pre-requisites
+    if isfield( param, 'debug' )
+        debug = param.debug;
+        if ~isa( outfile, 'logical' )
+            warning( 'Input argument outfile should be boolean')
+            debug = false;
+        end
+    end
+    
+    if isfield( param, 'outfile' )
+        outfile = param.outfile;
+        if ~isa( outfile, 'char' )
+            warning( 'Input argument outfile should be a string')
+            outfile = 'snippet.png';
+        end
+    end
+    
+    %check latex binary
+    if isfield( param, 'latex' )
+        latex = param.latex;
+    else
+        latex = '/usr/bin/latex';
+    end
+    
+    if ~exist( latex, 'file' )
+        error( ['latex2png: LaTeX was not located in ' latex '. To install ' ...
+            'LaTeX in Ubuntu type in terminal: sudo apt-get install ' ...
+            'tetex-base tetex-extra'] );
+    end
+    
+    %check dvipng binary
+    if isfield( param, 'dvipng' )
+        dvipng = param.dvipng;
+    else
+        dvipng = '/usr/bin/dvipng';
+    end
+    
+    if ~exist( dvipng, 'file' )
+        error( ['latex2png: dvipng was not located in ' dvipng '. To install ' ...
+            'dvipng in Ubuntu type in terminal: sudo apt-get install ' ...
+            'dvipng'] );
+    end
+    
+    %check convert binary
+    if isfield( param, 'convert' )
+        convert = param.convert;
+    else
+        convert = '/usr/bin/convert';
+    end
+    
+    if( ~exist( convert, 'file' ) )
+        error( ['latex2png: convert was not located in ' convert '. To install ' ...
+            'convert in Ubuntu type in terminal: sudo apt-get install ' ...
+            'imagemagick'] );
+    end
+else
+    error( ['latex2png: Wrong number of input arguments. Refer to ' ...
+        'documentation or visit http://latex2png.sourceforge.net or ' ...
+        'https://github.com/icaoberg/latex2png for more information']);
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%
+% WRITE TEMPORARY FILE %
+%%%%%%%%%%%%%%%%%%%%%%%%
+write_latex_file( snippet, outfile );
+
+%%%%%%%%%%%%%%%%%%%
+% CREATE DVI FILE %
+%%%%%%%%%%%%%%%%%%%
+system( [latex ' -interaction=nonstopmode ' outfile ' .tex'] );
+
+%%%%%%%%%%%%%%%%%%%%
+% PARSE DVI TO PNG %
+%%%%%%%%%%%%%%%%%%%%
+%if you want to make your image transparent change white to transparent
+system( [dvipng ' -q -T tight -bg white -D' density ' ' ...
+    outfile '.dvi'] );
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+% REMOVE TEMPORARY FILES %
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+delete( [ outfile '.tex' ] );
+delete( [ outfile '.dvi' ] );
+delete( [ outfile '.aux' ] );
+delete( [ outfile '.log' ] );
 end%latex2png
 
-function writeLaTeXfile( snippet, outfile )
+function write_latex_file( snippet, outfile )
     fileID = fopen( [outfile '.tex'] , 'w' );
     fprintf( fileID, '%s\n', '\documentclass[fleqn]{article}' );
     fprintf( fileID, '%s\n', '\usepackage{amssymb,amsmath,bm}' );
